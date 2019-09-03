2da1253f92462c6fe0c87a9721aba018649f0e05
yuanjiayang <yuanjiayang@qiezilife.com>
2019-09-03 17:45:13 +0800

    test v1

---
+++ examples/demo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: My
+ * Date: 2019/9/3
+ * Time: 17:20
+ */
+
+require_once dirname(__FILE__) .  '/../vendor/autoload.php';
+
+
+\Test\Yuan\Tools::hello();
---
+++ src/Yuan/Tools.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: My
+ * Date: 2019/9/3
+ * Time: 17:19
+ */
+
+namespace Test\Yuan;
+
+
+class Tools
+{
+    public function __construct()
+    {
+    }
+
+    public static function hello()
+    {
+        echo "hello word";
+    }
+}
